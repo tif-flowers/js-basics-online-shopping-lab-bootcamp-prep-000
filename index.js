b5494a9ed75e59fc7268413b032231d1688b2715
Tif Flowers <tif@gentlebull.com>
2018-03-13 13:45:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -80,6 +80,6 @@ function placeOrder(cardNumber) {
   
   else {
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
-    return deleteOrder;
+    return (cart = []);
   }
 }
