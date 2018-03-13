ef112069df50d27f346097e9b2754d6e829faf08
Tif Flowers <tif@gentlebull.com>
2018-03-13 13:28:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,13 +73,13 @@ function removeFromCart(item) {
 }
 }
 function placeOrder(cardNumber) {
-  var order = [];
+  var deleteOrder = [];
   if (cardNumber === undefined) {
     return console.log(`Sorry, we don't have a credit card on file for you.`);
   }
   
   else {
-    return console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
-    setCart(order)
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    setCart(deleteOrder);
   }
 }
